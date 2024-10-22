de449c595b25771e4e2585ede2743390ef6e4463
Jak0busus <focke.jakob@gmail.com>
2024-10-22 22:22:29 +0200

    Added description

--- src/main/java/de/jakob/Main.java
+++ src/main/java/de/jakob/Main.java
@@ -6,8 +6,8 @@ public class Main {
 
     public static void main(String [] args){
 
-        EndDateCalculator endDateCalculator = new EndDateCalculator();
-        endDateCalculator.prompt();
+        new EndDateCalculator().prompt();
+
     }
 
 }
--- src/main/java/de/jakob/tasks/EndDateCalculator.java
+++ src/main/java/de/jakob/tasks/EndDateCalculator.java
@@ -4,27 +4,37 @@ import java.util.Scanner;
 
 public class EndDateCalculator {
 
+    /**
+     *Implementieren Sie ein Programm, welches zu einem gegebenen Startdatum und einer gegebenen Anzahl t an
+     *Tagen ein Enddatum berechnet, sodass das Enddatum genau t Tage nach dem Startdatum liegt. Beispiels weise
+     * liegt der 30.10.2024 genau einen Tag nach dem 29.10.2024. Die Berechnung soll mithilfe einer einzigen
+     * geeigneten Schleife durchgeführt werden. Das Programm soll zudem weder break noch continue benutzen.
+     * Verwenden Sie erneut die Klasse SimpleIO zum Einlesen und Ausgeben von Werten.
+     * Das Programm fragt zunächst nach dem Startdatum. Hierzu wird der*die Benutzer*in nacheinander
+     * aufgefordert, den entsprechenden Tag des Monats, die Nummer des Monats und das Jahr einzugeben. Anschließend
+     * fragt das Programm nach der Anzahl t an Tagen. Alle Werte sollen als int eingelesen werden. In allen
+     * Berechnungen kann die Existenz von Schaltjahren vernachlässigt werden. Falls das eingegebene Startdatum nicht
+     * existiert oder t keine positive Zahl ist, so darf sich das Programm beliebig verhalten.
+     */
+
     private final static Scanner scanner = new Scanner(System.in);
-    private final static String PROMPT = "Bitte geben Sie die x des Startdatums ein:";
+    private final static String PROMPT = "Bitte geben Sie die %s des Startdatums ein:";
 
     public void prompt() {
 
-        int day = readInt(PROMPT.replace("x", "Tageskomponente")
+        int day = readInt(String.format(PROMPT, "Tageskomponente")
                 , 31);
 
-        int month = readInt(PROMPT.replace("x", "Monatskomponente")
+        int month = readInt(String.format(PROMPT, "Monatskomponente")
                 , 12);
 
-        int year = readInt(PROMPT.replace("x", "Jahreskomponente")
+        int year = readInt(String.format(PROMPT, "Jahreskomponente")
                 , 0);
+
         int t = readInt("Bitte geben Sie die Anzahl an Tagen ein:"
                 , 0);
 
-        if (nextMonth(day - 1, month)
-                || day == 0
-                || month == 0
-                || year == 0
-                || t == 0) {
+        if (isInvalidDate(day, month, year, t)) {
             System.out.println("Dieses Datum kann nicht existieren. Starte das Program erneut!");
             return;
         }
@@ -33,19 +43,16 @@ public class EndDateCalculator {
                 + calculate(day, month, year, t)
                 + " befindet sich "
                 + t
-                + " Tage nach dem Startdatum .\n");
+                + " Tage nach dem Startdatum.\n");
 
-        prompt();
     }
 
     private String calculate(int d, int m, int y, int t) {
 
         int years = y + t / 365;
-
         t = t % 365;
 
         int months = m;
-
         int days = d;
 
         for (int i = 0; i < t; i++) {
@@ -67,54 +74,48 @@ public class EndDateCalculator {
         }
 
         return days + "." + months + "." + years;
-
     }
 
     private boolean nextYear(int day, int month) {
-
         return month == 12 && day >= 31;
-
     }
 
     private boolean nextMonth(int day, int month) {
-
         switch (month) {
-
-            case 1, 3, 5, 7, 8, 10: return day >= 31;
-
-            case 4, 6, 9, 11: return day >= 30;
-
-            case 2: return day >= 29;
-
-            default: return false;
+            case 1, 3, 5, 7, 8, 10:
+                return day >= 31;
+            case 4, 6, 9, 11:
+                return day >= 30;
+            case 2:
+                return day >= 29;
+            default:
+                return false;
         }
+    }
 
+    private boolean isInvalidDate(int day, int month, int year, int t) {
+        return nextMonth(day - 1, month)
+                || day == 0
+                || month == 0
+                || year == 0
+                || t == 0;
     }
 
     private int readInt(String prompt, int limit) {
-
         int number = 0;
 
         System.out.print(prompt + "\n");
 
         if (scanner.hasNextInt()) {
-
             int i = scanner.nextInt();
             if (i > 0 &&
                     (limit == 0 || i <= limit)) {
 
                 number = i;
-
-            } else {
-                System.out.println("Das ist keine valide Zahl!");
-
             }
-
         } else {
             scanner.next();
-            System.out.println("Das ist keine Zahl!");
         }
-
         return number;
     }
 }
